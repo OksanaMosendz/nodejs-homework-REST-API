3ca1e69d26845042331d5bc3122a6c858f423b42
OksanaMosendz <bagheera03@gmail.com>
2021-12-07 02:00:18 +0200

    pagination done

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,10 +2,14 @@ const { Contacts } = require('../../models/index');
 
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const result = await Contacts.find({ owner: _id }).populate(
-    'owner',
-    '_id name email phone favorite',
-  );
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contacts.find({ owner: _id }, '', {
+    limit: Number(limit),
+    skip,
+  }).populate('owner', '_id name email phone favorite');
+
   res.status(200).json({
     status: 'success',
     code: 200,
